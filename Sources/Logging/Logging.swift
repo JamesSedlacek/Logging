0d5d2b6ea0377e7e67a4c4aaca05d8820b67df18
James Sedlacek <Jsedlacekjr@gmail.com>
2024-01-01 16:54:07 -0500

    Updated types

--- Sources/Logging/Logging.swift
+++ Sources/Logging/Logging.swift
@@ -11,7 +11,7 @@ import Foundation
 public macro Logging() = #externalMacro(module: "LoggingMacros", type: "LoggingMacro")
 
 @attached(member, names: named(logger))
-public macro LazyLogging() = #externalMacro(module: "LoggingMacros", type: "LoggingMacro")
+public macro LazyLogging() = #externalMacro(module: "LoggingMacros", type: "LazyLoggingMacro")
 
 @attached(member, names: named(logger))
-public macro ObservableLogging() = #externalMacro(module: "LoggingMacros", type: "LoggingMacro")
+public macro ObservableLogging() = #externalMacro(module: "LoggingMacros", type: "ObservableLoggingMacro")
